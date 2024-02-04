4d73a9917bbbc541c264a151ecf495057d5ca00f
saloni123ss <114285228+saloni123ss@users.noreply.github.com>
2024-02-04 20:22:32 +0530

    Add files via upload

---
+++ FCFS.cpp
@@ -0,0 +1,174 @@
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+
+using namespace std;
+
+// Structure to represent a process
+class process{
+ public:
+  int pid;     // Process ID
+  int at;      //Arrival Time
+  int bt;      //Burst Time
+//  int st;      //Start Time
+//  int ct;      //Completion Time
+//  int tat;     //Turn-Around Time
+//  int wt;      //Waiting Time
+};
+
+// Function to compare processes based on arrival time
+bool comapreArrival(process p1,process p2)
+{
+  return p1.at < p2.at;
+}
+
+// Function to compare processes based on Process Id
+bool compareID(process p1,process p2)
+{
+  return p1.pid < p2.pid;
+}
+
+int main()
+{
+  int n;
+  process p[100];
+  int ct[100],tat[100],st[100];
+  int wt[100];
+  float avg_tat,avg_wt;
+  int total_tat=0;
+  int total_wt=0;
+
+  cout<<setprecision(2)<<fixed;
+
+  cout<<"Enter the number of processes : ";
+  cin>>n;
+ 
+  //Input Process
+  for(int i=0;i<n;i++)
+  {
+    p[i].pid = i+1;
+    cout<<"Enter arrival time of process p"<<p[i].pid<<" : ";
+    cin>>p[i].at;
+
+    cout<<"Enter Burst time of process p"<<p[i].pid<<"   : ";
+    cin>>p[i].bt;
+    cout<<endl;
+  }
+
+  // Sort processes based on arrival time
+  sort(p,p+n,comapreArrival);
+  for(int i=0;i<n;i++)
+  {
+    st[i]=(i==0)?p[i].at:max(ct[i-1],p[i].at);
+    ct[i]=st[i] + p[i].bt;
+    tat[i]=ct[i] - p[i].at;
+    wt[i] = tat[i] - p[i].bt;
+
+    total_tat+=tat[i];
+    total_wt+=wt[i];
+  }
+
+  avg_tat = (float)total_tat/n;
+  avg_wt  = (float)total_wt/n;
+
+  //sort(p,p+n,compareID);
+  cout<<endl;
+
+  cout<<"#P\t"<<"AT\t"<<"BT\t"<<"CT\t"<<"TAT\t"<<"Wt\t"<<endl;
+  for(int i=0;i<n;i++)
+  {
+    cout<<p[i].pid<<"\t"<<p[i].at<<"\t"<<p[i].bt<<"\t"<<ct[i]<<"\t"<<tat[i]<<"\t"<<wt[i]<<endl;
+  } 
+
+  cout<<endl;
+  cout<<"Average TurnAround Time :: "<<avg_tat<<endl;
+  cout<<"Average Waiting Time    :: "<<avg_wt<<endl;
+  return 0;
+}
+
+
+/*
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+
+using namespace std;
+
+class process
+{
+ public:
+  int at;
+  int bt;
+  int pid;
+  int st;
+  int tat;
+  int wt;
+  int ct;
+};
+
+bool compareAt(process p1,process p2)
+{
+  return p1.at < p2.at;
+}
+
+bool compareId(process p1,process p2)
+{
+  return p1.pid < p2.pid;
+}
+
+int main()
+{
+  int n;
+  process p[100];
+  //int ct[100],tat[100],st[100],wt[100];
+  float tatavg,wtavg;
+  int total_tat=0,total_wt=0;
+
+  cout<<setprecision(2)<<fixed;
+  //cout<<setprecision(2)<<fixed;
+  cout<<"Enter no. of process : ";
+  cin>>n;
+
+  for(int i=0;i<n;i++)
+  {
+    p[i].pid=i+1;
+    cout<<"\nEnter at for p"<<p[i].pid<<" : ";
+    cin>>p[i].at;
+
+    cout<<"\nEnter bt for p"<<p[i].pid<<" : ";
+    cin>>p[i].bt;
+    cout<<endl;
+  }
+
+  sort(p,p+n,compareAt);
+  for(int i=0;i<n;i++)
+  {
+    p[i].st=(i==0)?p[i].at:max(p[i-1].ct,p[i].at);
+    p[i].ct=p[i].st+p[i].bt;
+    p[i].tat=p[i].ct-p[i].at;
+    p[i].wt=p[i].tat-p[i].bt;
+
+    total_tat+=p[i].tat;
+    total_wt+=p[i].wt;
+  }
+
+  sort(p,p+n,compareId);
+  //cout<<"\nwt = "<<total_wt<<endl;;
+  tatavg=(float)total_tat/n;
+  wtavg=(float)total_wt/n;
+
+  //cout<<"\nwt = "<<wtavg<<endl;
+
+  cout<<"#p"<<"\tAT"<<"\tBT"<<"\tCT"<<"\tWT"<<"\tTAT"<<endl;;
+
+  for(int i=0;i<n;i++)
+  {
+    cout<<p[i].pid<<"\t"<<p[i].at<<"\t"<<p[i].bt<<"\t"<<p[i].ct<<"\t"<<p[i].wt<<"\t"<<p[i].tat<<endl;
+  }
+
+  cout<<endl;
+  cout<<"avg tat : "<<tatavg<<endl;
+  cout<<"avg wt  : "<<wtavg<<endl;
+  return 0;
+}
+*/
